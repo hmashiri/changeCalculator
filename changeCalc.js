7c2885db34189a905c1855ee46a346c09087a274
Hadjir Mashiri <hmashiri@hotmail.com>
2016-09-27 16:43:02 -0700

    saving point

---
+++ changeCalc.js
@@ -0,0 +1,19 @@
+
+var saleAmount = $('#saleItemAmount').val();
+var customerGiveAmount = $('#customerPayAmount').val();
+var totalAmountDueBack = customerGiveAmount - saleAmount;
+
+for (var quarters = 0; 
+
+
+
+
+$("document").ready(function()
+{
+
+
+
+
+
+
+});
